9def149aaf7e816c3963c65afa5159041d9e1bef
BAHbKA007 <peniskopfbacken@gmail.com>
2019-09-05 22:39:35 +0200

    li größer auf Tablet

--- resources/views/zaehlung/zaehlung.blade.php
+++ resources/views/zaehlung/zaehlung.blade.php
@@ -7,7 +7,7 @@
     {{-- <p><a href="/zaehlung/{{$var['id']}}">{{$wochentag[strftime("%u", strtotime($var['zaehlung']->created_at))]}}</a> / <a href="#">?</a></p> --}}
     <div class="list-group">
         @foreach ($var['kunden'] as $item)
-            <a href="/zaehlung/{{$var['zaehlung']->id}}/kunde/{{$item->id}}" class="list-group-item list-group-item-action">
+            <a href="/zaehlung/{{$var['zaehlung']->id}}/kunde/{{$item->id}}" class="list-group-item list-group-item-action" style="padding-top: 12px;padding-bottom: 12px">
                 {{$item->name}} @if ($item->summe > 0)
                     <span class="badge badge-primary" style="margin-left:10px">{{$item->summe}} Kolli</span>
                 @endif 
--- resources/views/zaehlung/zaehlung_artikel.blade.php
+++ resources/views/zaehlung/zaehlung_artikel.blade.php
@@ -19,8 +19,8 @@ $wochentag = [
     <p><a href="/zaehlung/{{$var['zaehlung']->id}}">{{$wochentag[strftime("%u", strtotime($var['zaehlung']->created_at))]}}</a> / <a href=""><b>{{$var['kunde']->name}}</b></a></p>
     <div class="list-group">
         @foreach ($var['artikel'] as $item)
-            <a href="/zaehlung/{{$var['zaehlung']->id}}/kunde/{{$var['kunde']->id}}/artikel/{{$item->id}}" class="list-group-item list-group-item-action">
-                {{$item->bezeichnung}} @if ($item->summe > 0) <span class="badge badge-primary" style="margin-left:10px">{{$item->summe}} Kolli</span> @endif
+            <a href="/zaehlung/{{$var['zaehlung']->id}}/kunde/{{$var['kunde']->id}}/artikel/{{$item->id}}" class="list-group-item list-group-item-action" style="padding-top: 12px;padding-bottom: 12px">
+                {{$item->bezeichnung}} @if ($item->summe > 0) <span class="badge badge-primary" style="margin-left:10px;">{{$item->summe}} Kolli</span> @endif
             </a>
         @endforeach
     </div>
