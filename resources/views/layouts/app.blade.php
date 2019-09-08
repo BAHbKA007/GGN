59681e8d93970bbb901d02d596011a538a2523fe
Schneider <j.schneider@tomate.local>
2019-09-08 23:27:03 +0200

    k

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,14 +47,14 @@
     <title>{{ config('app.name', 'QM') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}?v=0.5"></script>
+    <script src="{{ asset('js/app.js') }}?v=0.6"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}?v=0.2" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}?v=0.3" rel="stylesheet">
 </head>
 <body>
     <div>
