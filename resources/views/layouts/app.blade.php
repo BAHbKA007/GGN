84432cac77a52ff4c526d9109e8f6789eb24ed02
Schneider <j.schneider@tomate.local>
2019-09-09 22:57:38 +0200

    Alert placement

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -54,7 +54,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}?v=0.5" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}?v=0.6" rel="stylesheet">
 </head>
 <body>
     <div>
