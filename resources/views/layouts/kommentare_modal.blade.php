34b1439331fa139f1cf3cf2e88de1a3520f2d34f
Schneider <j.schneider@tomate.local>
2019-09-11 19:14:05 +0200

    Kommentare bugfix

--- resources/views/layouts/kommentare_modal.blade.php
+++ resources/views/layouts/kommentare_modal.blade.php
@@ -37,15 +37,22 @@
 </div>
 @if (isset($var['kommentar_artikel']))
 <script>
+    var i = 0;
+    // falls Kommentar im Artikel erzeugt wird, die Artikelbezeichnung dem Text hinzufügen
     $('.unten_rechts').on('click', function () {
-        var text = $('#textarea').val();
-        if (text.length > 0) {
-            text = text.concat("\n", "{{$var['kommentar_artikel']->bezeichnung}}: ")
-        } else {
-            text = text.concat("{{$var['kommentar_artikel']->bezeichnung}}: ")
-        }
 
-        $('#textarea').val(text);
+        if (i == 0) {
+            var text = $('#textarea').val();
+            if (text.length > 0) {
+                text = text.concat("\n", "{{$var['kommentar_artikel']->bezeichnung}}: ")
+            } else {
+                text = text.concat("{{$var['kommentar_artikel']->bezeichnung}}: ")
+            }
+
+            $('#textarea').val(text);
+        };
+
+        i++;
     })
 </script>
 @endif
