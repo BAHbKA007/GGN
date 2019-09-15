5f36e66ea0ecd17cd4ba7d2790f4f31ff0cdbda6
Schneider <j.schneider@tomate.local>
2019-09-15 14:58:20 +0200

    test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,7 +12,7 @@
         <div class="col-md-12">
             <div class="card">
                 <div class="card-header">Hallo {{$var['user']}}</div>
-                <div class="card-body">{{$var['blalba']}}
+                <div class="card-body">
                     Am {{$wochentag[getdate(time())['wday']]}} den {{date("d.m.Y", time())}} um {{date("H:i", time())}} Uhr, KW: {{date('W')}}
                 </div>
             </div>
